dc01bd26e1f5856e91f27ad49e1a4cbb387f32db
OlioDiPalmas <oliodipalmas@icloud.com>
2018-06-02 12:54:52 +0200

    1.0-beta3: pulizia codice
    Sono stati rimossi molti ";" inutili

--- ntfs-macos.sh
+++ ntfs-macos.sh
@@ -1,169 +1,169 @@
 version=1.0-beta3
 homebrew_install() {
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bash_profile
-};
+}
 
 ntfs_3g_enable() {
-    echo "Inserisci il nome completo del tuo disco.";
-    echo "Per trovarlo, apri Utility Disco in Applicazioni > Utility dal Finder e vedi il nome della partizione (es. Macintosh HD)";
-    read disk;
+    echo "Inserisci il nome completo del tuo disco."
+    echo "Per trovarlo, apri Utility Disco in Applicazioni > Utility dal Finder e vedi il nome della partizione (es. Macintosh HD)"
+    read disk
 
     if [[ $(brew doctor) != "Your system is ready to brew." ]];
     then
         homebrew_install;
             if [[ $(brew doctor) == "Your system is ready to brew." ]];
             then
-                brew cask install osxfuse;
-                brew install ntfs-3g;
+                brew cask install osxfuse
+                brew install ntfs-3g
             else
-                echo "Errore 1: Homebrew non funzionante";
+                echo "Errore 1: Homebrew non funzionante"
                 echo "Controlla l'errore che ti dà e riavvia lo script";
                 exit 1;
-        fi;
+        fi
     else
-        brew cask install osxfuse;
-        brew install ntfs-3g;
-    fi;
+        brew cask install osxfuse
+        brew install ntfs-3g
+    fi
 
-    sudo mv "/Volumes/${disk}/sbin/mount_ntfs" "/Volumes/${disk}/sbin/mount_ntfs.orig";
-    sudo ln -s "/usr/local/sbin/mount_ntfs" "/Volumes/${disk}/sbin/mount_ntfs";
-    echo "" && echo "";
-    echo "Fatto! Ho abilitato la scrittura NTFS :)";
+    sudo mv "/Volumes/${disk}/sbin/mount_ntfs" "/Volumes/${disk}/sbin/mount_ntfs.orig"
+    sudo ln -s "/usr/local/sbin/mount_ntfs" "/Volumes/${disk}/sbin/mount_ntfs"
+    echo "" && echo ""
+    echo "Fatto! Ho abilitato la scrittura NTFS :)"
 
-    echo "Vuoi riavviare ora? (y/n)";
-    read reboot;
+    echo "Vuoi riavviare ora? (y/n)"
+    read reboot
 
     if [[ $reboot == "y" || $reboot == "Y" ]];
     then
-        echo "Va bene :^)";
-        sudo reboot;
-    fi;
+        echo "Va bene :^)"
+        sudo reboot
+    fi
 };
 
 ntfs_3g_disable() {
     echo "Vuoi davvero disabilitare la scrittura NTFS? (y/n)"
-    read okay;
+    read okay
     if [[ $okay == "y" || $okay == "Y" ]];
     then
-        echo "";
-        echo "Inserisci il nome completo del tuo disco.";
-        echo "Per trovarlo, apri Utility Disco in Applicazioni > Utility dal Finder e vedi il nome della partizione (es. Macintosh HD)";
-        read disk;
-        sudo mv "/Volumes/${disk}/sbin/mount_ntfs.orig" "/Volumes/${disk}/sbin/mount_ntfs";
-        brew uninstall ntfs-3g;
-        brew cask uninstall osxfuse;
+        echo ""
+        echo "Inserisci il nome completo del tuo disco."
+        echo "Per trovarlo, apri Utility Disco in Applicazioni > Utility dal Finder e vedi il nome della partizione (es. Macintosh HD)"
+        read disk
+        sudo mv "/Volumes/${disk}/sbin/mount_ntfs.orig" "/Volumes/${disk}/sbin/mount_ntfs"
+        brew uninstall ntfs-3g
+        brew cask uninstall osxfuse
 
-        echo "";
-        echo "Vuoi anche rimuovere Homebrew? (y/n)";
-        read uninstallbrew;
+        echo ""
+        echo "Vuoi anche rimuovere Homebrew? (y/n)"
+        read uninstallbrew
         if [[ $uninstallbrew == "y" || $uninstallbrew == "Y" ]];
         then
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)";
-        fi;
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+        fi
     else
-        ntfs_3g_menu;
-    fi;
+        ntfs_3g_menu
+    fi
 
-    echo "";
-    echo "Operazione completata.";
+    echo ""
+    echo "Operazione completata."
 
-    echo "Vuoi riavviare ora? (y/n)";
-    read reboot;
+    echo "Vuoi riavviare ora? (y/n)"
+    read reboot
     if [[ $reboot == "y" || $reboot == "Y" ]];
     then
-        echo "Va bene :^)";
-        sudo reboot;
-    fi;
+        echo "Va bene :^)"
+        sudo reboot
+    fi
 };
 
 fstab_enable() {
-    echo "Inserisci il nome completo del tuo disco (NON deve contenere spazi).";
-    echo "Per trovarlo, apri Utility Disco in Applicazioni > Utility dal Finder e vedi il nome della partizione (es. Macintosh HD)";
-    read disk;
-    echo "LABEL=${disk}  none    ntfs    rw,auto,nobrowse" | sudo tee -a /etc/fstab;
+    echo "Inserisci il nome completo del tuo disco (NON deve contenere spazi)."
+    echo "Per trovarlo, apri Utility Disco in Applicazioni > Utility dal Finder e vedi il nome della partizione (es. Macintosh HD)"
+    read disk
+    echo "LABEL=${disk}  none    ntfs    rw,auto,nobrowse" | sudo tee -a /etc/fstab
     sudo ln -s /Volumes ~/Desktop/Volumes
-    echo "";
-    echo "Fatto! Ho abilitato la scrittura NTFS :)";
+    echo ""
+    echo "Fatto! Ho abilitato la scrittura NTFS :)"
 
-    echo "Vuoi riavviare ora? (y/n)";
-    read reboot;
+    echo "Vuoi riavviare ora? (y/n)"
+    read reboot
     if [[ $reboot == "y" || $reboot == "Y" ]];
     then
-        echo "Va bene :^)";
-        sudo reboot;
-    fi;
+        echo "Va bene :^)"
+        sudo reboot
+    fi
 };
 
 fstab_disable() {
     echo "Continuando eliminerai completamente il file /etc/fstab. Continuare? (y/n)"
-    read delete;
+    read delete
     if [[ $delete == "y" || $delete == "Y" ]];
     then
-        echo "";
-        sudo rm /etc/fstab;
-        rm ~/Desktop/Volumes;
-        echo "";
-        echo "Operazione completata.";
+        echo ""
+        sudo rm /etc/fstab
+        rm ~/Desktop/Volumes
+        echo ""
+        echo "Operazione completata."
 
-        echo "Vuoi riavviare ora? (y/n)";
-        read reboot;
+        echo "Vuoi riavviare ora? (y/n)"
+        read reboot
         if [[ $reboot == "y" || $reboot == "Y" ]];
         then
-            echo "Va bene :^)";
-            sudo reboot;
-        fi;
+            echo "Va bene :^)"
+            sudo reboot
+        fi
     else
-        fstab_menu;
-    fi;
+        fstab_menu
+    fi
 };
 
 fstab_editor() {
-    clear;
-    echo "Scrivi il nome del text editor desiderato (es. nvim)";
-    read editor;
-    sudo ${editor} /etc/fstab;
-    clear;
-    echo "Finito di modificare? Vuoi ritornare al menu? (y/n)";
+    clear
+    echo "Scrivi il nome del text editor desiderato (es. nvim)"
+    read editor
+    sudo ${editor} /etc/fstab
+    clear
+    echo "Finito di modificare? Vuoi ritornare al menu? (y/n)"
     echo "Inviando 'q' uscirai dallo script"
-    read input;
-    if [[ $input == "y" || $input == "Y" ]]
+    read input
+    if [[ $input == "y" || $input == "Y" ]];
     then
-        fstab_edit;
-    else if [[ $input == "n" || $input == "N" ]]
+        fstab_edit
+    else if [[ $input == "n" || $input == "N" ]];
     then
-        echo "";
-        echo "Vuoi usare lo stesso text editor? (y/n)";
-        echo "Dopo che avrai finito, ritornerai al menu";
-        read input;
-        if [[ $input == "y" || $input == "Y" ]]
+        echo ""
+        echo "Vuoi usare lo stesso text editor? (y/n)"
+        echo "Dopo che avrai finito, ritornerai al menu"
+        read input
+        if [[ $input == "y" || $input == "Y" ]];
         then
-            sudo ${editor} /etc/fstab;
-            fstab_edit;
+            sudo ${editor} /etc/fstab
+            fstab_edit
         else
-            fstab_editor;
-        fi;
-        else if [[ $input == "q" || $input == "Q" ]]
+            fstab_editor
+        fi
+        else if [[ $input == "q" || $input == "Q" ]];
             then
-                clear;
-            fi;
-        fi;
-    fi;
+                clear
+            fi
+        fi
+    fi
 };
 
 ntfs_3g_menu() {
     clear
-    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗";
-    echo "║ Hai scelto: Homebrew e ntfs-3g                            ║";
-    echo "║ Scegli un'opzione                                         ║";
-    echo "╠═══╦═══════════════════════════════════════════════════════╣";
-    echo "║ 1 ║ Abilita scrittua NTFS                                 ║";
-    echo "║ 2 ║ Disabilita scrittura NTFS                             ║";
-    echo "║ 3 ║ Torna indietro                                        ║";
-    echo "║ 4 ║ Esci                                                  ║";
-    echo "╚═══╩═══════════════════════════════════════════════════════╝";
+    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗"
+    echo "║ Hai scelto: Homebrew e ntfs-3g                            ║"
+    echo "║ Scegli un'opzione                                         ║"
+    echo "╠═══╦═══════════════════════════════════════════════════════╣"
+    echo "║ 1 ║ Abilita scrittua NTFS                                 ║"
+    echo "║ 2 ║ Disabilita scrittura NTFS                             ║"
+    echo "║ 3 ║ Torna indietro                                        ║"
+    echo "║ 4 ║ Esci                                                  ║"
+    echo "╚═══╩═══════════════════════════════════════════════════════╝"
 
-    read input;
+    read input
     case ${input} in
         "1")
             clear;
@@ -181,22 +181,22 @@ ntfs_3g_menu() {
 };
 
 fstab_menu() {
-    clear;
-    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗";
-    echo "║ Hai scelto: fstab                                         ║";
-    echo "║                                                           ║";
-    echo "║      ATTENZIONE: leggi il README prima di continuare      ║";
-    echo "║              USALO A TUO RISCHIO E PERICOLO!              ║";
-    echo "║                                                           ║";
-    echo "║ Scegli un'opzione                                         ║";
-    echo "╠═══╦═══════════════════════════════════════════════════════╣";
-    echo "║ 1 ║ Abilita scrittua NTFS                                 ║";
-    echo "║ 2 ║ Disabilita scrittura NTFS                             ║";
-    echo "║ 3 ║ Modifica manualmente /etc/fstab                       ║";
-    echo "║ 4 ║ Torna indietro                                        ║";
-    echo "║ 5 ║ Esci                                                  ║";
-    echo "╚═══╩═══════════════════════════════════════════════════════╝";
-    read input;
+    clear
+    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗"
+    echo "║ Hai scelto: fstab                                         ║"
+    echo "║                                                           ║"
+    echo "║      ATTENZIONE: leggi il README prima di continuare      ║"
+    echo "║              USALO A TUO RISCHIO E PERICOLO!              ║"
+    echo "║                                                           ║"
+    echo "║ Scegli un'opzione                                         ║"
+    echo "╠═══╦═══════════════════════════════════════════════════════╣"
+    echo "║ 1 ║ Abilita scrittua NTFS                                 ║"
+    echo "║ 2 ║ Disabilita scrittura NTFS                             ║"
+    echo "║ 3 ║ Modifica manualmente /etc/fstab                       ║"
+    echo "║ 4 ║ Torna indietro                                        ║"
+    echo "║ 5 ║ Esci                                                  ║"
+    echo "╚═══╩═══════════════════════════════════════════════════════╝"
+    read input
     case ${input} in
         "1")
             clear;
@@ -212,22 +212,22 @@ fstab_menu() {
             clear;;
         *)
             fstab_menu;;
-    esac;
+    esac
 };
 
 fstab_edit() {
-    clear;
-    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗";
-    echo "║ Vuoi modificare il file /etc/fstab                        ║";
-    echo "║ Con quale text editor vuoi aprirlo?                       ║";
-    echo "╠═══╦═══════════════════════════════════════════════════════╣";
-    echo "║ 1 ║ nano (consigliato)                                    ║";
-    echo "║ 2 ║ vim                                                   ║";
-    echo "║ 3 ║ emacs                                                 ║";
-    echo "║ 4 ║ Altro                                                 ║";
-    echo "║ 5 ║ Torna indietro                                        ║";
-    echo "╚═══╩═══════════════════════════════════════════════════════╝";
-    read input;
+    clear
+    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗"
+    echo "║ Vuoi modificare il file /etc/fstab                        ║"
+    echo "║ Con quale text editor vuoi aprirlo?                       ║"
+    echo "╠═══╦═══════════════════════════════════════════════════════╣"
+    echo "║ 1 ║ nano (consigliato)                                    ║"
+    echo "║ 2 ║ vim                                                   ║"
+    echo "║ 3 ║ emacs                                                 ║"
+    echo "║ 4 ║ Altro                                                 ║"
+    echo "║ 5 ║ Torna indietro                                        ║"
+    echo "╚═══╩═══════════════════════════════════════════════════════╝"
+    read input
     case ${input} in
         "1")
             clear;
@@ -247,28 +247,28 @@ fstab_edit() {
             fstab_menu;;
         *)
             fstab_edit;;
-    esac;
+    esac
 };
 
 about() {
-    clear;
-    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗";
-    echo "║ Script creato da gstux e OlioDiPalmas per OpenSlime.it    ║";
-    echo "║                                                           ║";
-    echo "║ Versione script: ${version}                                ║";
-    echo "║                                                           ║";
-    echo "║ Questo script è distribuito sotto licenza MIT             ║";
-    echo "║ Puoi guardarla dal file LICENSE oppure inviando 1         ║";
-    echo "║                                                           ║";
-    echo "║ https://github.com/gstux                                  ║";
-    echo "║ https://github.com/OlioDiPalmas                           ║";
-    echo "║ https://github.com/OpenSlime                              ║";
-    echo "╠═══╦═══════════════════════════════════════════════════════╣";
-    echo "║ 1 ║ Vedi ora la licenza                                   ║";
-    echo "║ 2 ║ Torna indietro                                        ║";
-    echo "╚═══╩═══════════════════════════════════════════════════════╝";
+    clear
+    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗"
+    echo "║ Script creato da gstux e OlioDiPalmas per OpenSlime.it    ║"
+    echo "║                                                           ║"
+    echo "║ Versione script: ${version}                                ║"
+    echo "║                                                           ║"
+    echo "║ Questo script è distribuito sotto licenza MIT             ║"
+    echo "║ Puoi guardarla dal file LICENSE oppure inviando 1         ║"
+    echo "║                                                           ║"
+    echo "║ https://github.com/gstux                                  ║"
+    echo "║ https://github.com/OlioDiPalmas                           ║"
+    echo "║ https://github.com/OpenSlime                              ║"
+    echo "╠═══╦═══════════════════════════════════════════════════════╣"
+    echo "║ 1 ║ Vedi ora la licenza                                   ║"
+    echo "║ 2 ║ Torna indietro                                        ║"
+    echo "╚═══╩═══════════════════════════════════════════════════════╝"
 
-    read input;
+    read input
     case ${input} in
         "1")
             license;;
@@ -276,40 +276,40 @@ about() {
             main_menu;;
         *)
             about;;
-    esac;
+    esac
 };
 
 license() {
-    clear;
+    clear
     cat LICENSE
-    echo "";
-    echo "╔═══╦══════════════════╗";
-    echo "║ 1 ║ Torna indietro   ║";
-    echo "╚═══╩══════════════════╝";
+    echo ""
+    echo "╔═══╦══════════════════╗"
+    echo "║ 1 ║ Torna indietro   ║"
+    echo "╚═══╩══════════════════╝"
 
-    read input;
+    read input
     case ${input} in
         "1")
             about;;
         *)
             license;;
-    esac;
+    esac
 };
 
 main_menu() {
-    clear;
-    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗";
-    echo "║ Benvenuto/a :)                                            ║";
-    echo "║ Scegli il metodo che vuoi utilizzare per abilitare        ║";
-    echo "║ la scrittura NTFS nel tuo macOS                           ║";
-    echo "╠═══╦════════════════════════════════╦══════════════════════╣";
-    echo "║ 1 ║ Homebrew e ntfs-3g             ║ lungo, ma stabile    ║";
-    echo "║ 2 ║ fstab                          ║ veloce, ma instabile ║";
-    echo "║ 3 ║ Informazioni                   ╚══════════════════════╣";
-    echo "║ 4 ║ Esci                                                  ║";
-    echo "╚═══╩═══════════════════════════════════════════════════════╝";
+    clear
+    echo "╔═══════════════════════╡ OpenSlime ╞═══════════════════════╗"
+    echo "║ Benvenuto/a :)                                            ║"
+    echo "║ Scegli il metodo che vuoi utilizzare per abilitare        ║"
+    echo "║ la scrittura NTFS nel tuo macOS                           ║"
+    echo "╠═══╦════════════════════════════════╦══════════════════════╣"
+    echo "║ 1 ║ Homebrew e ntfs-3g             ║ lungo, ma stabile    ║"
+    echo "║ 2 ║ fstab                          ║ veloce, ma instabile ║"
+    echo "║ 3 ║ Informazioni                   ╚══════════════════════╣"
+    echo "║ 4 ║ Esci                                                  ║"
+    echo "╚═══╩═══════════════════════════════════════════════════════╝"
 
-    read input;
+    read input
     case ${input} in
         "1")
             ntfs_3g_menu;;
@@ -321,7 +321,7 @@ main_menu() {
             clear;;
         *)
             main_menu;;
-    esac;
+    esac
 };
 
-main_menu;
+main_menu
